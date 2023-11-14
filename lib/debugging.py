543b0cd09eb8eaae733f5b5ec62c1ad93b67bf44
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2023-11-14 10:45:37 +0000

    debugging challenge

--- lib/debugging.py
+++ lib/debugging.py
@@ -9,47 +9,64 @@
 
 ## Exercise 2
 
-def encode(text, key):
-    cipher = make_cipher(key)
-    # print(cipher)
+# def encode(text, key):
+#     cipher = make_cipher(key)
+#     # print(cipher)
 
-    ciphertext_chars = []
-    for i in text:
-        ciphered_char = chr(65 + cipher.index(i))
-        ciphertext_chars.append(ciphered_char)
+#     ciphertext_chars = []
+#     for i in text:
+#         ciphered_char = chr(65 + cipher.index(i))
+#         ciphertext_chars.append(ciphered_char)
 
-    return "".join(ciphertext_chars)
-
-
-def decode(encrypted, key):
-    cipher = make_cipher(key)
-
-    plaintext_chars = []
-    for i in encrypted:
-        plain_char = cipher[ord(i) - 65]
-        plaintext_chars.append(plain_char)
-
-    return "".join(plaintext_chars)
+#     return "".join(ciphertext_chars)
 
 
-def make_cipher(key):
-    alphabet = [chr(i + 96) for i in range(1, 27)]
-    # print('alphabet: ', alphabet)
-    cipher_with_duplicates = list(key) + alphabet
+# def decode(encrypted, key):
+#     cipher = make_cipher(key)
 
-    cipher = []
-    for i in range(0, len(cipher_with_duplicates)):
-        if cipher_with_duplicates[i] not in cipher_with_duplicates[:i]:
-            cipher.append(cipher_with_duplicates[i])
+#     plaintext_chars = []
+#     for i in encrypted:
+#         plain_char = cipher[ord(i) - 65]
+#         plaintext_chars.append(plain_char)
 
-    return cipher
+#     return "".join(plaintext_chars)
 
-# When you run this file, these next lines will show you the expected
-# and actual outputs of the functions above.
-print(f"""Running: encode("theswiftfoxjumpedoverthelazydog", "secretkey")
-Expected: EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL Actual: {encode('theswiftfoxjumpedoverthelazydog', 'secretkey')}
-""")
 
-print(f"""Running: decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
-Expected: theswiftfoxjumpedoverthelazydog Actual: {decode('EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL', 'secretkey')}
+# def make_cipher(key):
+#     alphabet = [chr(i + 96) for i in range(1, 27)]
+#     # print('alphabet: ', alphabet)
+#     cipher_with_duplicates = list(key) + alphabet
+
+#     cipher = []
+#     for i in range(0, len(cipher_with_duplicates)):
+#         if cipher_with_duplicates[i] not in cipher_with_duplicates[:i]:
+#             cipher.append(cipher_with_duplicates[i])
+
+#     return cipher
+
+# # When you run this file, these next lines will show you the expected
+# # and actual outputs of the functions above.
+# print(f"""Running: encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# Expected: EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL Actual: {encode('theswiftfoxjumpedoverthelazydog', 'secretkey')}
+# """)
+
+# print(f"""Running: decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# Expected: theswiftfoxjumpedoverthelazydog Actual: {decode('EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL', 'secretkey')}
+# """)
+
+
+## Challenge
+def get_most_common_letter(text):
+    counter = {}
+    for char in text:
+        if char.isalpha():
+            counter[char] = counter.get(char, 0) + 1
+    letter = sorted(counter.items(), key=lambda item: item[1])[-1][0]
+    return letter
+
+
+print(f"""
+Running:  get_most_common_letter("the roof, the roof, the roof is on fire!"))
+Expected: o
+Actual:   {get_most_common_letter("the roof, the roof, the roof is on fire!")}
 """)
